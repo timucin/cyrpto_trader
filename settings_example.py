eab73bd74938c1163baa0a250bb68efc1ca8a342
Timucin <timucinkizilay@gmail.com>
2017-10-25 15:01:56 +0300

    created example settings.py

---
+++ settings_example.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# rename this file to settings.py and enter the api keys for poloniex
+
+polo_api_key = "super_rich_acounts_api_key"
+polo_api_secret = "super_secret_top_secret"
